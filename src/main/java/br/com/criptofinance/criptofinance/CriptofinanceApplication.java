a35767d2fb808aa46558207d48fea032dd9f111a
Geraldo Felix Jr <geraldosilvajr06@gmail.com>
2026-08-13 22:59:32 -0300

    Setup inicial do projeto -  Spring Boot

---
+++ src/main/java/br/com/criptofinance/criptofinance/CriptofinanceApplication.java
@@ -0,0 +1,13 @@
+package br.com.criptofinance.criptofinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptofinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptofinanceApplication.class, args);
+	}
+
+}
