60a7dadc76d704092c788167583154b4e6d08a2c
Geraldo Felix Jr <geraldosilvajr06@gmail.com>
2026-08-22 15:40:42 -0300

    Add WalletPolicy

---
+++ bd/policy/WalletPolicy.sql
@@ -0,0 +1,17 @@
+ALTER TABLE carteira ENABLE ROW LEVEL SECURITY;
+
+create policy carteira_insert
+  on carteira for insert
+  to authenticated
+  with check(auth.uid() = id_usuario);
+
+create policy carteira_update
+  on carteira for update
+                                    to authenticated
+                                    using (auth.uid() = id_usuario)
+                  with check(auth.uid() = id_usuario);
+
+create policy carteira_select
+  on carteira for select
+                             to authenticated
+                             using (auth.uid() = id_usuario);
