a2bba5d1170604485ff218c1a26ca1410aad123b
Gabriel Melo <gabsmelo19.dev@gmail.com>
2026-08-26 21:29:14 -0300

    Update and rename CryptoPolicy.sql to PolicyCryptocurrency.sql
    Alterando o nome para PolicyCryptocurrency

--- bd/policy/CryptoPolicy.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE criptomoeda ENABLE ROW LEVEL SECURITY;
-
-GRANT SELECT ON criptomoeda TO anon, authenticated;
-
-create policy criptomoeda_select
-  on criptomoeda for select
-                                to anon, authenticated
-                                using (true); -- todos podem acessar a table cripto, tanto anons quanto logados
