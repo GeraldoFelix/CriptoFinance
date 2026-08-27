1838491e79ea4acca7dc0d6c04f976c709d1cdf6
Gabriel Melo <gabsmelo19.dev@gmail.com>
2026-08-26 21:25:21 -0300

    Update and rename TransationPolicy.sql to PolicyTransation.sql
    Alterando o nome para PolicyTransation

---
+++ bd/policy/PolicyTransation.sql
@@ -0,0 +1,26 @@
+ALTER TABLE transacao ENABLE ROW LEVEL SECURITY;
+
+GRANT select, insert on transacao to authenticated;
+
+CREATE POLICY transacao_select
+    ON transacao FOR SELECT
+    TO authenticated
+    USING (
+      EXISTS (
+        SELECT 1 FROM carteira
+        WHERE (carteira.id_carteira = transacao.id_carteira_origem
+               OR carteira.id_carteira = transacao.id_carteira_destino)
+               AND carteira.id_usuario = auth.uid()
+      )
+    );
+
+CREATE POLICY transacao_insert
+    ON transacao FOR INSERT
+    TO authenticated
+    WITH CHECK (
+        EXISTS (
+            SELECT 1 FROM carteira
+            WHERE carteira.id_carteira = transacao.id_carteira_origem
+            AND carteira.id_usuario = auth.uid()
+        )
+    );
