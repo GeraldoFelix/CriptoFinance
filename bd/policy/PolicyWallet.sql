322f1fcf4992e0e12a24364cbe69ff6ed3c247aa
Gabriel Melo <gabsmelo19.dev@gmail.com>
2026-08-26 21:30:16 -0300

    Update and rename WalletPolicy.sql to PolicyWallet.sql
    Alterando o nome para PolicyWallet

--- bd/policy/PolicyWallet.sql
+++ bd/policy/PolicyWallet.sql
@@ -4,18 +4,18 @@ GRANT SELECT, update, insert ON carteira TO authenticated;
 
 create policy carteira_insert
   on carteira for insert
-  to authenticated
+  to authenticated 
   with check(auth.uid() = id_usuario);
 
 create policy carteira_update
   on carteira for update
-                                    to authenticated
-                                    using (auth.uid() = id_usuario)
-                  with check(auth.uid() = id_usuario);
+  to authenticated
+  using (auth.uid() = id_usuario)
+  with check(auth.uid() = id_usuario);
 
 create policy carteira_select
   on carteira for select
-                             to authenticated
-                             using (auth.uid() = id_usuario);
+  to authenticated 
+  using (auth.uid() = id_usuario);
 
 -- terminado
